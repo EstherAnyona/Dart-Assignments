8e3e86e2b7cc7ce124ce621a02a2d8d7752cc825
Esther Murunga <144281904+EstherAnyona@users.noreply.github.com>
2024-03-04 16:44:25 +0300

    Add files via upload

---
+++ Datatypes.dart
@@ -0,0 +1,21 @@
+void main() {
+  // Demonstrating int data type
+  int age = 25;
+  print('Age: $age');
+
+  //Demonstrating double data type
+  double weight = 65.5;
+  print('Weight: $weight');
+
+  //Demonstrating String data type
+  String name = 'Esther Murunga';
+  print('Name: $name');
+
+  //Demonstrating List data type
+  List<String> fruits = ['Apple', 'Banana', 'Orange'];
+  print('Fruits: $fruits');
+
+  //Demonstrating Map data type
+  Map<String, int> studentScores = {'Math': 90, 'Science': 85, 'English': 88};
+  print('Student Scores; $studentScores');
+}
